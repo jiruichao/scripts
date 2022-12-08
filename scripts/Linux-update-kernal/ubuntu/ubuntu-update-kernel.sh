9e729e79f4e9f242e2ccab2d3fa70efe62dd6b35
jiruichao <74128725+jiruichao@users.noreply.github.com>
2022-12-08 10:57:06 +0800

    Update ubuntu-update-kernel.sh

--- scripts/Linux-update-kernal/ubuntu/ubuntu-update-kernel.sh
+++ scripts/Linux-update-kernal/ubuntu/ubuntu-update-kernel.sh
@@ -6,13 +6,13 @@
 #官方地址: https://kernel.ubuntu.com/~kernel-ppa/mainline/ 
 
 #lt 长期支持版
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-modules-5.5.207-0504207-generic_5.4.207-0504207.202207211701_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-modules-5.4.207-0504207-lowlatency_5.4.207-0504207.202207211701_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-image-unsigned-5.4.207-0504207-lowlatency_5.4.207-0504207.202207211701_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-image-unsigned-5.4.207-0504207-generic_5.4.207-0504207.202207211701_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-headers-5.4.207-0504207_5.4.207-0504207.202207211701_all.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-headers-5.4.207-0504207-lowlatency_5.4.207-0504207.202207211701_amd64.deb
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-headers-5.4.207-0504207-generic_5.4.207-0504207.202207211701_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-modules-5.5.207-0504207-generic_5.4.207-0504207.202207211701_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-modules-5.4.207-0504207-lowlatency_5.4.207-0504207.202207211701_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-image-unsigned-5.4.207-0504207-lowlatency_5.4.207-0504207.202207211701_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-image-unsigned-5.4.207-0504207-generic_5.4.207-0504207.202207211701_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-headers-5.4.207-0504207_5.4.207-0504207.202207211701_all.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-headers-5.4.207-0504207-lowlatency_5.4.207-0504207.202207211701_amd64.deb
+#wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.4.207/amd64/linux-headers-5.4.207-0504207-generic_5.4.207-0504207.202207211701_amd64.deb
 
 #安装新版本内核
 dpkp -i *.deb
